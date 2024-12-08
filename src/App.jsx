bde0fb04b5f932be39ff71020f8b176d23beffcb
Maksouni <max1205325@gmail.com>
2024-12-08 23:10:11 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import './styles.css'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a href='' className="logo-text">
+                    <span style={{ color: 'var(--secondary-color)' }}>C</span>
+                    o
+                    <span style={{ color: 'var(--primary-color)' }}>D</span>
+                    e
+                </a>
+                <ul>
+                    <li>
+                        <a href="">Чему вы научитесь</a>
+                    </li>
+                    <li>
+                        <a href="">Процесс обучения</a>
+                    </li>
+                    <li>
+                        <a href="">Стоимость</a>
+                    </li>
+                    <li>
+                        <a href="">Контакты</a>
+                    </li>
+                </ul>
+                <a href='tel:+7 499 348 93 96' className='phone'>+7 (499) 348 93 96</a>
+            </nav>
+        </header>
+    )
+}
