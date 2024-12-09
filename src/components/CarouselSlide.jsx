e7e6498c86c1b6d670c1d04fc938d03bc43b27ca
Maksouni <max1205325@gmail.com>
2024-12-09 15:02:26 +0700

    custom button

--- src/components/CarouselSlide.jsx
+++ src/components/CarouselSlide.jsx
@@ -1,23 +1,33 @@
 import Code from '../assets/code.svg'
+import CustomButton from './CustomButton'
+
 export default function CarouselSlide() {
+    const handleButtonClick = () => {
+
+    }
+
     return (
         <div className="carousel-slide">
             <img src={Code} alt="code" className='image-slide' />
             <div className='slide-text-container'>
                 <h1>
-                    Школа 
+                    Школа
                     <span style={{ color: 'var(--primary-color)' }}>{` {`}</span>
                     <span style={{ fontWeight: 700 }}>программирования</span>
                     <span style={{ color: 'var(--primary-color)' }}>{`} `}</span>
                     <br />для тех, кому нужны
-                    <br />реальные навыки, 
+                    <br />реальные навыки,
                     <br />а не просто сертификат
                 </h1>
                 <hr />
                 <p>
-                Закажите звонок, и наш специалист свяжется с
-                <br />вами в ближайшее время
+                    Закажите звонок, и наш специалист свяжется с
+                    <br />вами в ближайшее время
                 </p>
+                <CustomButton
+                    text='Заказать звонок'
+                    onClick={handleButtonClick}
+                />
             </div>
         </div>
     )
