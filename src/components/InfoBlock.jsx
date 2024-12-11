56fc73b2b2a821fc42167aa6af492d73a1bae514
Maksouni <max1205325@gmail.com>
2024-12-11 12:43:58 +0700

    adaptive

--- src/components/InfoBlock.jsx
+++ src/components/InfoBlock.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 // eslint-disable-next-line react/prop-types
 export default function InfoBlock({image, title, text}){
     return(
-        <div className="text-block">
+        <div className="info-block">
             <img src={image} alt="image" />
             <h3 className='title'>{title}</h3>
             <p className='text'>{text}</p>
